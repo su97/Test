c2afbab223c5bf4623a643cef41f78724d7ad0e2
Cuisir <1174988626@qq.com>
2016-08-25 10:24:03 +0800

    second try

--- app/src/main/java/android/ten/Fragment1_1.java
+++ app/src/main/java/android/ten/Fragment1_1.java
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -22,6 +24,9 @@ public class Fragment1_1 extends Fragment {
 
 
     private NewsInfo info;
+    private TextView author,title,authorbrief,text1,text2,text3,text4,text5;
+    private ImageView image1,image2,image3,image4;
+    private View view;
 
     public Fragment1_1() {
         // Required empty public constructor
@@ -51,6 +56,7 @@ public class Fragment1_1 extends Fragment {
                     info.setText3(json.getString("text3"));
                     info.setText4(json.getString("text4"));
                     info.setText5(json.getString("text5"));
+                    info.setTitle(json.getString("title"));
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -84,7 +90,43 @@ public class Fragment1_1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment1_1, container, false);
+        view = inflater.inflate(R.layout.fragment_fragment1_1, container, false);
+        initData();
+        initView();
+
+
+        return view;
+    }
+
+    private void initData() {
+        author.setText(info.getAuthor());
+        authorbrief.setText(info.getAuthorbrief());
+        title.setText(info.getTitle());
+        text1.setText(info.getText1());
+        text2.setText(info.getText2());
+        text3.setText(info.getText3());
+        text4.setText(info.getText4());
+        text5.setText(info.getText5());
+        String str1 = info.image1;
+        String str2 = info.image2;
+        String str3 = info.image3;
+        String str4 = info.image4;
+    }
+
+    private void initView() {
+        author = (TextView) view.findViewById(R.id.author);
+        authorbrief = (TextView) view.findViewById(R.id.authorbrief);
+        title = (TextView) view.findViewById(R.id.title);
+        text1 = (TextView) view.findViewById(R.id.text1);
+        text2 = (TextView) view.findViewById(R.id.text2);
+        text3 = (TextView) view.findViewById(R.id.text3);
+        text4 = (TextView) view.findViewById(R.id.text4);
+        text5 = (TextView) view.findViewById(R.id.text5);
+        image1 = (ImageView) view.findViewById(R.id.img1);
+        image2 = (ImageView) view.findViewById(R.id.img2);
+        image3 = (ImageView) view.findViewById(R.id.img3);
+        image4 = (ImageView) view.findViewById(R.id.img4);
+
     }
 
 
